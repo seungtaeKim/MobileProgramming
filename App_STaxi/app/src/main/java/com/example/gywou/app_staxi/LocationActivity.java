babe936ba8f93c14a35e1638ebebecbe81af15fa
CHANWOONG\gywou <gywou0309@koreatech.ac.kr>
2016-12-17 01:16:01 +0900

    TermProject_v1

---
+++ App_STaxi/app/src/main/java/com/example/gywou/app_staxi/LocationActivity.java
@@ -0,0 +1,8 @@
+package com.example.gywou.app_staxi;
+
+/**
+ * Created by gywou on 2016-12-17.
+ */
+
+public class LocationActivity {
+}
---
+++ App_STaxi/app/src/main/java/com/example/gywou/app_staxi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gywou.app_staxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //getSupportActionBar().setBackgroundDrawable();
+        setContentView(R.layout.login);
+    }
+}
