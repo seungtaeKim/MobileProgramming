911a4d2610f78de8d85618d8480acd6992a69e97
CHANWOONG\gywou <gywou0309@koreatech.ac.kr>
2016-12-17 00:51:27 +0900

    TermProject_v1

---
+++ TermProject_v1/STaxi2/app/src/main/java/com/example/gywou/staxi/boardActivity.java
@@ -0,0 +1,30 @@
+package com.example.gywou.staxi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by gywou on 2016-12-16.
+ */
+
+public class boardActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.board);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){  //옵션 메뉴 생성
+        getMenuInflater().inflate(R.menu.write_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){    //옵션 메뉴 누를 시
+        switch(item.getItemId()){
+            case R.id.write_bar:    //작성 바 누를 시
+            default:    //디폴드 값 설정
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
